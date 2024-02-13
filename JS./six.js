9c9809068ed61171cea36745d2822ea6e1be7046
Vinod SIngh <115033744+Vinod123456183@users.noreply.github.com>
2024-02-13 23:19:26 +0530

    Create six.js

---
+++ JS./six.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <script>
+
+        // diff bet var let const
+
+        // in js array and obj are obj 
+
+        // var c = 10;
+    // var c=20;
+    // c = 111;
+    // console.log(c);
+
+
+    // let x = 10;
+    // x= 20; iske aage let nahi
+    // console.log(x);
+
+
+    // const p = 11;
+    // p=44;   na iska naam change na val
+    // console.log(p);
+
+    </script>
+
+</body>
+</html>
