719f81b723ca2a2a2a5e17a0d7dfdd50eaed4007
Vinod SIngh <115033744+Vinod123456183@users.noreply.github.com>
2024-02-13 23:17:27 +0530

    Create one.js

---
+++ JS./one.js
@@ -0,0 +1,24 @@
+// Js By Yahoo Baba
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- Ecternal js  -->
+    <script src="1.js"></script> <!--To link java Script-->
+
+
+</head>
+<body>
+    
+
+    <!-- Internal Js -->
+    <script>
+        document.write("<i>Hello Form Internal Js</i>\n");//vid 2 script k andr html
+    </script>
+
+
+</body>
+</html>
