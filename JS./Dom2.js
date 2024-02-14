3820c9c636ff2d6d56f461e382f9fe1a18886973
Vinod SIngh <115033744+Vinod123456183@users.noreply.github.com>
2024-02-15 00:12:05 +0530

    Create Dom2.js

---
+++ JS./Dom2.js
@@ -0,0 +1,18 @@
+// console.log(document.getElementById("iid").innerText);
+// console.log(document.getElementById("iid").innerHTML);
+// console.log(document.getElementById("iid").getAttribute("class")); /*Note Class and idd  */
+// console.log(document.getElementById("iid").getAttribute("style"));
+// console.log(document.getElementById("iid").getAttribute("click"));
+// console.log(document.getElementById("iid").getAttributeNode("click"));
+// console.log(document.getElementById("iid").attributes);
+// console.log(document.getElementById("iid").attributes[0]);
+// console.log(document.getElementById("iid").attributes[1]);
+// console.log(document.getElementById("iid").attributes[0].value);
+// console.log(document.getElementById("iid").attributes[1].name);
+
+
+// Dom Set Method
+
+document.getElementById("iid").innerText = "JAI SHREE RAM";
+console.log(document.getElementById("iid").innerHTML);
+// get set attribute , remove  not done
