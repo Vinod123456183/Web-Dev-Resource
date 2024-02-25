18cfa281191b2997e006e69c9bfdce7102df4090
Vinod SIngh <115033744+Vinod123456183@users.noreply.github.com>
2024-02-25 15:15:01 +0530

    Create 6-1.js

---
+++ Node-JS-(Code_Step_By_Step)/6-1.js
@@ -0,0 +1,8 @@
+// global -? jise import krne ki need nahi h
+// !global -? jise import krne ki need   h
+
+const fs = require('fs');
+fs.writeFileSync("code.txt","f") //part of file system
+
+const fs = require('fs').writeFileSync;
+fs("code.txt","f") //part of file system
