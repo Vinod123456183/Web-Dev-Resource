bcac8fa3bbb8e7d62cf36ca2f9e480d05965020c
Vinod SIngh <115033744+Vinod123456183@users.noreply.github.com>
2024-02-26 07:08:25 +0530

    Create 7_to_11.js

---
+++ Node-JS-(Code_Step_By_Step)/7_to_11.js
@@ -0,0 +1,31 @@
+// package .json file
+
+// y project ki detail rkhti h code se related  version , version , package , information 
+
+// how to make
+
+// To create json npm   { init }
+
+// npm i colors package 2 
+
+// npm i simple-node-logger
+
+// package.json nahi hone chaie delete
+
+// node js is single thrreaded
+
+
+// Nod emodule git me push nahi krna chaiyie
+var colors = require('colors');
+console.log("sds".bgCyan);
+
+
+
+
+console.log("sd sds ");
+console.log("sd sds sd");
+// saves time
+// nodemon .\filename
+
+// interview 
+// node js is async and single threaded language 
