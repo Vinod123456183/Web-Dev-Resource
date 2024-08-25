b3c2f4a0a334787bc0ed6fde20d724b45878229d
Vinod SIngh <115033744+Vinod123456183@users.noreply.github.com>
2024-08-26 00:26:06 +0530

    Create Phone.jsx

---
+++ REACT_TAILWIND_COMPONENTS/Small Small Components/Phone-Aside-Relative-Absolute/Phone.jsx
@@ -0,0 +1,5 @@
+
+<div className=" flex relative w-[50%] p-1 bg-red-400  ">
+          <img className="absolute z-50 min-w-[350px] w-40  right-0" src={img1} alt={'img'} />
+          <img className="absolute min-w-[300px] w-36 top-14 right-0" src={img2} alt={'img'} />
+</div>
