86d466b2825a16fd1c8439e0c0b15a8d35f9f428
Vinod SIngh <115033744+Vinod123456183@users.noreply.github.com>
2024-08-27 07:40:00 +0100

    Create main.jsx

---
+++ Framer-Motion-Help/Div-From-right-to-left/main.jsx
@@ -0,0 +1,15 @@
+<motion.h1
+  initial={{ opacity: 0, rotateX: '70deg', scaleY: 0.4, x: 50 }}
+  animate={{ opacity: 1, rotateX: '0deg', scaleY: 1, x: 0 }}
+  transition={{ duration: 0.5 }}
+  className="m-0 text-[3.5rem] md:text-[4rem] lg:text-[4.5rem] xl:text-[7.8rem] text-white  "
+  >
+  <span>Track Crypto</span>
+</motion.h1>
+
+
+  and later give delay in different different by 
+
+  .5 , .9 , 1.5
+
+  transition={{ duration: 0.5 , delay : .9}}
